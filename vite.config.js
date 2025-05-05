f289255fb11bfd76633175bc661f6f189667c5b1
erintrliu <erinliutr@gmail.com>
2025-05-04 23:12:24 -0700

    update package

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+// Replace 'your-repo-name' with the actual name of your GitHub repository
+export default defineConfig({
+  base: '/crochetable/', // <-- important for GitHub Pages
+});
